0f603345f1201e559234abf5c347b71a787a685b
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-07 19:24:52 +0200

    add form and pug

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world');
+app.get('/', function(req, res){
+    res.render('dynamic')
 });
 
-app.get('/store', function (req, res) {
-    res.send('sklep');
+app.get('/next?', function (req, res) {
+    res.send('Its alive!');
 });
 
 app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
